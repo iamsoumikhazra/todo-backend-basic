03900b7b728be074a8c03e48837689e392e6d46b
iamsoumikhazra <www.soumikhazra@gmail.com>
2024-05-18 20:13:10 +0530

    replacing sync code with async code

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  let todo = '<a href="http://localhost:3000/todos">View All</a>';
-  let addTodo = '<a href="http://localhost:3000/addtodo">Add Todo</a>';
-  let deleteTodo = '<a href="http://localhost:3000/delete-todo/:id">Delete Todo</a>';
-  let updateTodo = '<a href="http://localhost:3000/update-todo/:id">Update Todo</a>';
+  let todo = '<button><a href="http://localhost:3000/todos" >View All</a></button>';
+  let addTodo = '<button><a href="http://localhost:3000/addtodo">Add Todo</a></button>';
+  let deleteTodo = '<button><a href="http://localhost:3000/delete-todo/:id">Delete Todo</a></button>';
+  let updateTodo = '<button><a href="http://localhost:3000/update-todo/:id">Update Todo</a></button>';
   res.send(todo + " " + addTodo + " " + deleteTodo + " " + updateTodo);
 });
 
@@ -21,23 +21,23 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
-app.post("/addtodo", (req, res) => {
-  const existingData = fs.readFileSync("./todo.json", "utf8");
+app.post("/addtodo", async(req, res) => {
+  const existingData = await fs.readFileSync("./todo.json", "utf8");
   const existingObject = JSON.parse(existingData);
   const newMessage = req.body;
   existingObject.messages.push(newMessage);
   const updatedData = JSON.stringify(existingObject, null, 2);
-  fs.writeFileSync("./todo.json", updatedData);
+  await fs.writeFileSync("./todo.json", updatedData);
   res.json({ success: true });
 });
 
-app.get("/todos", (req, res) => {
-  const data = fs.readFileSync("./todo.json", "utf8");
+app.get("/todos", async(req, res) => {
+  const data = await fs.readFileSync("./todo.json", "utf8");
   const object = JSON.parse(data);
   res.json(object);
 });
-app.get("/delete-todo/:id", (req, res) => {
-  const data = fs.readFileSync("./todo.json", "utf8");
+app.get("/delete-todo/:id", async(req, res) => {
+  const data = await fs.readFileSync("./todo.json", "utf8");
   const object = JSON.parse(data);
   const id = req.params.id;
   let deletedMessage = null;
@@ -49,12 +49,15 @@ app.get("/delete-todo/:id", (req, res) => {
     }
   }
   const updatedData = JSON.stringify(object, null, 2);
-  fs.writeFileSync("./todo.json", updatedData);
+  await fs.writeFileSync("./todo.json", updatedData);
+  if(!deletedMessage) return res.json({ success: false, error: "No Todo Found" });
+  else{
   res.json({ success: true, deletedMessage });
+  }
 });
 
-app.put("/update-todo/:id", (req, res) => {
-  const data = fs.readFileSync("./todo.json", "utf8");
+app.put("/update-todo/:id", async(req, res) => {
+  const data = await fs.readFileSync("./todo.json", "utf8");
   const object = JSON.parse(data);
   const id = req.params.id;
   const updatedMessage = req.body;
@@ -65,6 +68,9 @@ app.put("/update-todo/:id", (req, res) => {
     }
   }
   const updatedData = JSON.stringify(object, null, 2);
-  fs.writeFileSync("./todo.json", updatedData);
+  await fs.writeFileSync("./todo.json", updatedData);
+  if(!updatedMessage) return res.json({ success: false, error: "No Todo Found" });
+  else{
   res.json({ success: true , updatedMessage});
+  }
 });
